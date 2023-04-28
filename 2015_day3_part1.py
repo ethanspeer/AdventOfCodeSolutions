68a76f429ad5dcf7e3c04fa3557a7a51712bb8e3
ethanspeer <speer.ethan717@gmail.com>
2023-04-28 17:05:06 -0400

    day 3

---
+++ 2015_day3_part1.py
@@ -0,0 +1,24 @@
+puzzle_input = input("Enter your puzzle input file: ")
+
+current_x = 0
+current_y = 0
+
+point_presents = {(0,0)}
+
+with open(puzzle_input, "r") as file:
+    file_contents = file.read()
+
+for i in range(len(file_contents)):
+    char = file_contents[i]
+    if char == "^":
+        current_y += 1
+    elif char == "v":
+        current_y -= 1
+    elif char == ">":
+        current_x += 1
+    elif char == "<":
+        current_x -= 1
+    if (current_x, current_y) not in point_presents:
+        point_presents.update({(current_x, current_y)})
+
+print("Answer:", len(point_presents))
