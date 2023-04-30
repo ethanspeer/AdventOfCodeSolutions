a6fadb0488981ded2b64fe63c6fe76635e129291
ethanspeer <speer.ethan717@gmail.com>
2023-04-30 11:37:27 -0400

    day 4

---
+++ 2015_day4_part2.py
@@ -0,0 +1,18 @@
+import hashlib
+import sys
+
+puzzle_input = input("Enter your puzzle input: ")
+placeholder = puzzle_input
+my_string_bytes = puzzle_input.encode('utf-8')
+md5_hash = ""
+
+for i in range(sys.maxsize):
+    puzzle_input += str(i+1)
+    bytes = puzzle_input.encode('utf-8')
+    md5_hash = hashlib.md5(bytes).hexdigest()
+    puzzle_input = placeholder
+    if(md5_hash[:6] == "000000"):
+        print("Answer:", i+1)
+        break
+
+
